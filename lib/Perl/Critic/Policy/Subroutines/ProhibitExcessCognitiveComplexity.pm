7dd03430b7ec1c6c6f7e939a18ed159c2d35c004
Oliver Trosien <otrosien@epages.com>
2017-01-02 09:53:54 +0100

    example 1 + 2

--- lib/Perl/Critic/Policy/Subroutines/ProhibitExcessCognitiveComplexity.pm
+++ lib/Perl/Critic/Policy/Subroutines/ProhibitExcessCognitiveComplexity.pm
@@ -9,54 +9,76 @@ use base 'Perl::Critic::Policy';
 our $VERSION = '0.1';
 
 Readonly::Scalar my $DESC => q{Avoid excessive cognitive complexity};
-Readonly::Scalar my $EXPL =>
-q{See https://blog.sonarsource.com/cognitive-complexity-because-testability-understandability/};
+Readonly::Scalar my $EXPL => q{See https://blog.sonarsource.com/cognitive-complexity-because-testability-understandability/};
 
 sub supported_parameters {
-	return ();
+    return ( {
+            name            => 'warn_level',
+            description     => 'The complexity score allowed before warning starts.',
+            default_string  => '10',
+            behavior        => 'integer',
+            integer_minimum => 1,
+        },
+    );
 }
 
 sub default_severity {
-	return $SEVERITY_MEDIUM;
+    return $SEVERITY_MEDIUM;
 }
 
 sub default_themes {
-	return qw( complexity maintenance );
+    return qw( complexity maintenance );
 }
 
 sub applies_to {
-	return 'PPI::Statement::Sub';
+    return 'PPI::Statement::Sub';
 }
 
 sub violates {
-	my ( $self, $elem, undef ) = @_;
+    my ( $self, $elem, undef ) = @_;
 
-	return;
+    my @viols = ();
+
+    # minimum complexity is 0
+    my $score = $self->nested_complexity( $elem->find_first('PPI::Structure::Block'), 0);
+
+    if ( $score > 0 ) {
+        my $desc;
+        if ( my $name = $elem->name() ) {
+            $desc = qq<Subroutine "$name" with high complexity score ($score)>;
+        }
+        else {
+            $desc = qq<Anonymous subroutine with high complexity score ($score)>;
+        }
+
+        push @viols, Perl::Critic::Violation->new( $desc, $EXPL, $elem, ( $score >= $self->{'_warn_level'} ? $self->get_severity() : $SEVERITY_LOWEST ) );
+    }
+
+    return @viols;
 }
 
-package Complexity;
+sub nested_complexity {
+    my $self = shift;
+    my ( $elem, $nesting ) = @_;
 
-use overload '""' => \&toString;
+    return 0 unless ( $elem->can('schildren') );
 
-sub new {
-	my $class = shift;
-	my ( $nesting, $complexity ) = @_;
-	return bless { 'nesting' => $nesting, 'complexity' => $complexity }, $class;
-}
+    my $complexity = 0;
 
-sub increment_nesting {
-	my $self = shift;
-	return Complexity->new( $self->{'nesting'} + 1, $self->{'complexity'} );
-}
-
-sub increment_complexity {
-	my $self = shift;
-	return Complexity->new( $self->{'nesting'}, $self->{'complexity'} + 1 );
-}
-
-sub toString {
-	my $self = shift;
-	return "" + $self->{'complexity'};
+    for my $child ( $elem->schildren() ) {
+        #my $inc = 0;
+        if (   $child->isa('PPI::Structure::Given')
+            || $child->isa('PPI::Structure::For')
+            || $child->isa('PPI::Structure::Condition') )
+        {
+            $complexity += $nesting + 1; # aniticipate nesting increment
+        }
+        elsif ( $child->isa('PPI::Statement::Break') && ! scalar $child->find( sub { $_[1]->content eq 'return' }) ) {
+            $complexity++;
+        }
+        $complexity += $self->nested_complexity( $child, $nesting + $child->isa('PPI::Statement::Compound') ? 1 : 0 );
+    }
+    return $complexity;
 }
 
 1;
--- t/10_checks.t
+++ t/10_checks.t
@@ -1,6 +1,8 @@
 use strict;
 use Test::More;
 use Perl::Critic::TestUtils qw( pcritique pcritique_with_violations );
+use Perl::Critic::Policy::Subroutines::ProhibitExcessCognitiveComplexity;
+use Perl::Critic::Utils qw{ :severities };
 
 my $policy = 'Perl::Critic::Policy::Subroutines::ProhibitExcessCognitiveComplexity';
 
@@ -10,29 +12,55 @@ my $code = <<'END_CODE';
 sub a { }
 END_CODE
 my $violation_count = pcritique( $policy, \$code );
-is($violation_count, 0, 'No violation');
-
+is( $violation_count, 0, 'No violation' );
 
 ### example 1
 $code = <<'END_CODE';
-use feature 'switch';
+use experimental qw(switch);
                                 # Cyclomatic Complexity    Cognitive Complexity
-  sub getWords { #             +1
+
+sub getWords {                  #          +1
     my ($number) = @_;
     given ($number) {           #                                  +1
-      when 1:                   #          +1
+      when (1)                  #          +1
         { return "one"; }
-      when 2:                   #          +1
+      when (2)                  #          +1
         { return "a couple"; }
-      default:                  #          +1
+      default                   #          +1
         { return "lots"; }
     }
-  }                             #          =4                      =1
+}                               #          =4                      =1
+END_CODE
+my @violations = pcritique_with_violations( $policy, \$code );
+is(scalar @violations, 1, 'Found 1 violation');
+
+my $violation = $violations[0];
+is($violation->severity(), $SEVERITY_LOWEST, 'getWords: Violation is info-level');
+is($violation->description(), 'Subroutine "getWords" with high complexity score (1)', 'getWords: Violation description');
+
+### example 2
+$code = <<'END_CODE';
+
+sub sumOfPrimes {
+    my ($max) = @_;
+    my $total = 0;
+    OUT: for (my $i = 1; $i <= $max; ++$i) {
+        for (my $j = 2; $j < $i; ++$j) {
+            if ($i % $j == 0) {
+                 last OUT;
+            }
+        }
+        $total += $i;
+    }
+    return $total;
+}
 
 END_CODE
-my $aViolations = pcritique_with_violations( $policy, \$code );
-# WIP
-# is(scalar $aViolations, 1, 'Info-level violation');
+my @violations = pcritique_with_violations( $policy, \$code );
+is(scalar @violations, 1, 'Found 1 violation');
 
+my $violation = $violations[0];
+is($violation->severity(), $SEVERITY_LOWEST, 'sumOfPrimes: Violation is info-level');
+is($violation->description(), 'Subroutine "sumOfPrimes" with high complexity score (7)', 'sumOfPrimes: Violation description');
 
 done_testing();
