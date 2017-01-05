ed6c68c3f7e9584583380315a6391a96b48e3f5e
Oliver Trosien <otrosien@epages.com>
2017-01-05 18:42:13 +0100

    Checking in changes prior to tagging of version 0.03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 1fcdf9b..41532fd 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,8 @@
     Revision history for PerlCritic extension Perl-Critic-CognitiveComplexity
    
     {{$NEXT}}
    +
    +0.03 2017-01-05T17:40:36Z
         - support complexity increase for boolean operators
    
     0.02 2017-01-03T16:11:41Z

--- lib/Perl/Critic/Policy/CognitiveComplexity/ProhibitExcessCognitiveComplexity.pm
+++ lib/Perl/Critic/Policy/CognitiveComplexity/ProhibitExcessCognitiveComplexity.pm
@@ -8,7 +8,7 @@ use Perl::Critic::Utils qw{ :severities :classification :ppi };
 use Perl::Critic::Violation;
 use base 'Perl::Critic::Policy';
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 Scalar my $EXPL => q{Avoid code that is nested, and thus difficult to grasp.};
 Readonly my %BOOLEAN_OPS => map { $_ => 1 } qw( && || and or );
